45918e8c3ee05a9932d4c852575d16e77e490446
John Clayton <johnclayton72@gmail.com>
2024-03-09 22:30:47 +0100

    Move old files into archived/ folder.

--- src/strip-hiyaaa-versions.py
+++ src/strip-hiyaaa-versions.py
@@ -3,6 +3,6 @@ with open('../hiyaaa.txt', 'r') as f:
 
 package_names = [line.split('==')[0] for line in lines]
 
-with open('../requirements-wsl2.txt', 'w') as f:
+with open('../achived/requirements-wsl2.txt', 'w') as f:
     for package in package_names:
         f.write(package + '\n')
